fa672568658c0b22e4226bed533c2d0ae97c8a25
Maura Joglekar <m.joglekar@f5.com>
2021-04-08 14:32:42 -0700

    add httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item/media-item.component';
@@ -22,7 +23,8 @@ import { lookupLists, lookupListToken } from './providers'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [
     {provide: lookupListToken, useValue: lookupLists }
