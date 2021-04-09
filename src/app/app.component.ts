82868309937493ff8aaacbebebff5592aac999fe
Maura Joglekar <m.joglekar@f5.com>
2021-04-09 11:46:55 -0700

    added encapsulation to show how it can be changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 // template and styles can be used instead of templateUrl and stylesUtls
 // to put the html and CSS direclty here
 @Component({
   selector: 'mw-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.Emulated  //default
 })
 export class AppComponent {
   title = 'mediaWatch';
